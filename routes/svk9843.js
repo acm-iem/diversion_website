897b126e40ad333fc5399240c0288658b99239c6
souvik <souvikaasds01@gmail.com>
2022-01-13 21:57:53 +0530

    new projects added

--- routes/svk9843.js
+++ routes/svk9843.js
@@ -37,6 +37,59 @@ router.get('/participants',(req,res)=>{
      })
 })
 router.get('/search',(req,res)=>{
-     res.render('searchdb')
+     newUser.find((err,result)=>{
+          if(!err)
+               res.render('searchdb',{result:result})
+          else
+               throw err
+     })
+})
+router.get('/srchpro',(req,res)=>{
+
+     console.log(req.body)
+     if(req.body.cat == 'pcipantp'){
+          newpcipant.estimatedDocumentCount((err,n)=>{
+          count = n
+          // console.log(n)
+          })
+          qr1 = { pcipant_project:req.body.pcipant_project }
+          newpcipant.find(qr1).then((result)=>{
+               res.render("participantsdb",{pras:result,count:count})
+          })
+     }
+     else if(req.query.p == 'pro')
+     {
+          newUser.estimatedDocumentCount((err,n)=>{
+          count = n
+          // console.log(n)
+          })
+          qr = { project_category:req.query.cat }
+          newUser.find(qr).then((result)=>{
+               res.render("projectsdb",{pros:result,count:count})
+          })
+     }
+     // else if(req.query.p == 'pra')
+     // {
+     //      newpcipant.estimatedDocumentCount((err,n)=>{
+     //      count = n
+     //      // console.log(n)
+     //      })
+     //      qr = { pcipant_project:req.query.pro }
+     //      newpcipant.find(qr).then((result)=>{
+     //           res.render("participantsdb",{pras:result,count:count})
+     //      })
+     // }
+})
+router.get('/srchpra',(req,res)=>{
+
+     if(req.query.p == 'pro')
+     {
+          qr = { project_status:req.query.cat }
+     }
+     else if(req.query.p == 'pra')
+     {
+          qr = { pcipant_status:req.query.pro }
+     }
+     // console.log(qr)
 })
 module.exports = router
